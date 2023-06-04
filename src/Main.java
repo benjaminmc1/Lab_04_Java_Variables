123da74ccd34199e15887572e59c9578951654b5
benjaminmc1 <bmckee566@gmail.com>
2023-06-03 20:56:09 -0400

    Part 1

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,63 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Part 1
+        int intOperandA = 22;
+        int intOperandB = 19;
+        int intSum;
+        int intProduct;
+        int intDifference;
+        int intQuotient;
+        int intModulo;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        double doubleOperandA = 3.47;
+        double doubleOperandB = 2.56;
+        double doubleSum;
+        double doubleProduct;
+        double doubleDifference;
+        double doubleQuotient;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        intSum = intOperandA + intOperandB; // Assignment that uses the arithmetic operator +
+        System.out.println("The sum using ints of " + intOperandA + " " + intOperandB + " is " + intSum); // Output statement
+
+        intProduct = intOperandA * intOperandB; // Assignment that uses the arithmetic operator *
+        System.out.println("The product using ints of " + intOperandA + " " + intOperandB + " is " + intProduct); // Output statement
+
+        intDifference = intOperandA - intOperandB; // Assignment that uses the arithmetic operator -
+        System.out.println("The difference using ints of " + intOperandA + " " + intOperandB + " is " + intDifference); // Output statement
+
+        intQuotient = intOperandA / intOperandB; // Assignment that uses the arithmetic operator /
+        System.out.println("The quotient using ints of " + intOperandA + " " + intOperandB + " is " + intQuotient); // Output statement
+
+        intModulo = intOperandA % intOperandB; // Assignment that uses the arithmetic operator %
+        System.out.println("The remainder using ints of " + intOperandA + " " + intOperandB + " is " + intModulo); // Output statement
+
+        doubleSum = doubleOperandA + doubleOperandB; // Assignment that uses the arithmetic operator +
+        System.out.println("The sum using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleSum); // Output statement
+
+        doubleProduct = doubleOperandA * doubleOperandB; // Assignment that uses the arithmetic operator *
+        System.out.println("The product using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleProduct); // Output statement
+
+        doubleDifference = doubleOperandA - doubleOperandB; // Assignment that uses the arithmetic operator -
+        System.out.println("The difference using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleDifference); // Output statement
+
+        doubleQuotient = doubleOperandA / doubleOperandB; // Assignment that uses the arithmetic operator /
+        System.out.println("The quotient using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleQuotient); //Output statement
+
+        // Part 2
+        int numberOfSiblings = 4;
+        System.out.println("I have " + numberOfSiblings + " siblings."); // Output statement
+
+        double myShoeSize = 11;
+        System.out.println("I wear size " + myShoeSize + " shoes."); // Output statement
+
+        String myFullName = "Benjamin Robert McKee";
+        System.out.println("My full name is " + myFullName +  "."); // Output statement
+
+        int dayOfTheWeek = 3;
+        System.out.println("It is the " + dayOfTheWeek + "rd day of the week."); // Output statement
+
+        boolean isItThursday = false;
+        System.out.println("My coworker asked me earlier if it was Thursday. I told him that that was " + isItThursday + "."); // Output statement
     }
 }
